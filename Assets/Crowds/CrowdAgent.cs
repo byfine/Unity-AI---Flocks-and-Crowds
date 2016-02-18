d255967741a2b562bf73e0aa18f8c72a71b6fb19
byfine <7byfine@gmail.com>
2016-02-18 23:24:46 +0800

    add crowd demo

---
+++ Assets/Crowds/CrowdAgent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class CrowdAgent : MonoBehaviour {
+     
+    public Transform Target;
+
+    private NavMeshAgent agent;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = Random.Range(4.0f, 5.0f);
+        agent.SetDestination(Target.position);
+	}
+}
